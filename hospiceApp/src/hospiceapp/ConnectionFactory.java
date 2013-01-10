49a56a1760b75a199ee9aae9b4d8c6b2bfab2b67
workstation <workstation@workstation-PC>
2013-01-10 15:54:39 +0200

    ConnectionFactory is added

---
+++ hospiceApp/src/hospiceapp/ConnectionFactory.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospiceapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author workstation
+ */
+public class ConnectionFactory {
+    String driverClassName = "org.h2.Driver";
+        String connectionUrl = "jdbc:h2:test";
+        String dbUser = "sa";
+        String dbPwd = "";
+
+        private static ConnectionFactory connectionFactory = null;
+
+        private ConnectionFactory() {
+                try {
+                        Class.forName(driverClassName);
+                } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                }
+        }
+
+        public Connection getConnection() throws SQLException {
+                Connection conn = null;
+                conn = DriverManager.getConnection(connectionUrl, dbUser, dbPwd);
+                return conn;
+        }
+
+        public static ConnectionFactory getInstance() {
+                if (connectionFactory == null) {
+                        connectionFactory = new ConnectionFactory();
+                }
+                return connectionFactory;
+        }
+}
